6d72c0dededdc6c082f4bc4e02001fcc08d89d03
Bocchio01 <tommaso.bocchietti@gmail.com>
2025-01-27 23:42:43 +0100

    Reports completed

---
+++ 04 - 2D distributed systems/Main.m
@@ -0,0 +1,100 @@
+clc
+clear variables
+close all
+
+%% Plots setup
+
+reset(0)
+set(0, 'DefaultFigureNumberTitle', 'off');
+set(0, 'DefaultFigureWindowStyle', 'docked');
+set(0, 'defaultaxesfontsize', 15);
+set(0, 'DefaultLineLineWidth', 2);
+set(0, 'DefaultFigureColormap', gray)
+
+
+%% First requests
+
+figure_p = figure('Name', 'Changing p');
+tiledlayout(3, 1)
+
+for p = [1.0 2.0 3.0]
+    
+    rmin = 1.5;
+    nexttile
+    imagesc(-TO1_distributed(60, 20, 0.5, p, rmin))
+    title(['p=' num2str(p) ' | ' 'r_{min}=' num2str(rmin)])
+    axis equal
+    axis tight
+
+end
+
+
+figure_rmin = figure('Name', 'Changing rmin');
+tiledlayout(3, 1)
+
+for rmin = [1.0 1.2 1.5]
+    
+    p = 3.0;
+    nexttile
+    imagesc(-TO1_distributed(60, 20, 0.5, p, rmin))
+    title(['p=' num2str(p) ' | ' 'r_{min}=' num2str(rmin)])
+    axis equal
+    axis tight
+
+end
+
+
+%% Second request
+
+figure_load_case_2 = figure('Name', 'Fixed left side and extremity force');
+tiledlayout(2, 1)
+
+nexttile
+imagesc(-TO2_distributed(32, 20, 0.4, 3.0, 1.2))
+title(['p=' num2str(3.0) ' | ' 'r_{min}=' num2str(1.2)])
+axis equal
+axis tight
+
+
+%% Third request
+
+figure_load_case_3 = figure('Name', 'Fixed left side and extremity force');
+tiledlayout(2, 4)
+
+nexttile
+imagesc(-TO31_distributed(30, 30, 0.4, 3.0, 1.2))
+title(['p=' num2str(3.0) ' | ' 'r_{min}=' num2str(1.2)])
+axis equal
+axis tight
+
+nexttile
+imagesc(-TO32_distributed(30, 30, 0.4, 3.0, 1.2))
+title(['p=' num2str(3.0) ' | ' 'r_{min}=' num2str(1.2)])
+axis equal
+axis tight
+
+
+%% Fourth request
+
+figure_load_case_4 = figure('Name', 'Fixed left side and extremity force');
+tiledlayout(2, 1)
+
+nexttile
+imagesc(-TO4_distributed(45, 30, 0.5, 3.0, 1.5))
+title(['p=' num2str(3.0) ' | ' 'r_{min}=' num2str(1.5)])
+axis equal
+axis tight
+
+
+%% Exports
+
+plot_struct.export_path = 'latex/img/MATLAB';
+plot_struct.data = cell(0);
+
+plot_struct.data{end+1} = {figure_p, '/req_1p'};
+plot_struct.data{end+1} = {figure_rmin, '/req_1rmin'};
+plot_struct.data{end+1} = {figure_load_case_2, '/req_2'};
+plot_struct.data{end+1} = {figure_load_case_3, '/req_3'};
+plot_struct.data{end+1} = {figure_load_case_4, '/req_4'};
+
+export_pdf_figure(plot_struct);
---
+++ 04 - 2D distributed systems/functions/TO4_distributed.m
@@ -0,0 +1,153 @@
+function x = TO4_distributed(nx, ny, alpha, p, rmin)
+
+x(1:ny,1:nx) = alpha;
+change = 1.;
+
+passive = zeros(ny, nx);
+for ely = 1:ny
+    for elx = 1:nx
+        if sqrt((ely-ny/2.)^2+(elx-nx/3.)^2) < ny/3.
+            passive(ely,elx) = 1;
+            x(ely,elx) = 0.001;
+        end
+    end
+end
+
+while change > 0.01
+    xold = x;
+
+    % FEM Analysis
+    U = compute_U(nx, ny, x, p);
+
+    % Sensitivity Analysis
+    [KE] = compute_k_local();
+    c = 0.;
+    for ely = 1:ny
+        for elx = 1:nx
+            n1 = (ny+1)*(elx-1)+ely;
+            n2 = (ny+1)* elx   +ely;
+            Ue = U([ ...
+                2*n1-1; ...
+                2*n1; ...
+                2*n2-1; ...
+                2*n2; ...
+                2*n2+1; ...
+                2*n2+2; ...
+                2*n1+1; ...
+                2*n1+2 ...
+                ], 1);
+            c = c + x(ely,elx)^p * Ue' * KE * Ue;
+            dc(ely,elx) = -p*x(ely,elx)^(p-1) * Ue' * KE * Ue;
+        end
+    end
+
+    % Filtering Sensitivities
+    dc = apply_filter_to_dc(nx, ny, rmin, x, dc);
+
+    % Design variable update
+    x = optimality_criteria(nx, ny, x, alpha, dc, passive);
+
+    % colormap(gray)
+    % imagesc(-x)
+    % axis equal
+    % axis tight
+    % pause(1e-6);
+
+    change = max(max(abs(x - xold)));
+end
+
+end
+
+%%%%%%%%%% OPTIMALITY CRITERIA UPDATE %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+function xnew = optimality_criteria(nx, ny, x, alpha, dc, passive)
+
+l1 = 0; l2 = 100000; move = 0.2;
+while (l2-l1 > 1e-4)
+    lmid = 0.5*(l2+l1);
+    xnew = max(0.001,max(x-move,min(1.,min(x+move,x.*sqrt(-dc./lmid)))));
+    xnew(passive == 1) = 0.001;
+    if sum(sum(xnew)) - alpha*nx*ny > 0;
+        l1 = lmid;
+    else
+        l2 = lmid;
+    end
+end
+end
+
+
+function dcn = apply_filter_to_dc(nx, ny, rmin, x, dc)
+
+dcn = zeros(ny, nx);
+
+for x_idx = 1:nx
+    for y_idx = 1:ny
+
+        sum = 0.0;
+
+        for k = max(x_idx-floor(rmin),1):min(x_idx+floor(rmin),nx)
+            for l = max(y_idx-floor(rmin),1):min(y_idx+floor(rmin),ny)
+
+                fac = rmin-sqrt((x_idx-k)^2+(y_idx-l)^2);
+                sum = sum + max(0, fac);
+                dcn(y_idx,x_idx) = dcn(y_idx,x_idx) + max(0,fac)*x(l,k)*dc(l,k);
+
+            end
+        end
+
+        dcn(y_idx,x_idx) = dcn(y_idx,x_idx)/(x(y_idx,x_idx)*sum);
+
+    end
+end
+
+end
+
+
+
+function U = compute_U(nx, ny, x, p)
+
+k_local = compute_k_local();
+
+K = sparse(2*(nx+1)*(ny+1), 2*(nx+1)*(ny+1));
+F = sparse(2*(ny+1)*(nx+1), 1);
+U = zeros(2*(ny+1)*(nx+1), 1);
+
+for x_idx = 1:nx
+    for y_idx = 1:ny
+        n1 = (ny+1)*(x_idx-1) + y_idx;
+        n2 = (ny+1)* x_idx    + y_idx;
+        edof = [2*n1-1; 2*n1; 2*n2-1; 2*n2; 2*n2+1; 2*n2+2; 2*n1+1; 2*n1+2];
+        K(edof, edof) = K(edof, edof) + x(y_idx,x_idx)^p*k_local;
+    end
+end
+
+% Define loads & boundary conditions
+F(2*(nx+1)*(ny+1),1) = -1;
+fixeddofs   = 1:2*(ny+1);
+alldofs     = 1:2*(ny+1)*(nx+1);
+freedofs    = setdiff(alldofs,fixeddofs);
+
+% Solve for displacements
+U(freedofs, :) = K(freedofs,freedofs) \ F(freedofs,:);
+U(fixeddofs, :) = 0;
+
+end
+
+
+function k_local = compute_k_local()
+
+E = 1;
+nu = 0.3;
+
+k = [1/2-nu/6 1/8+nu/8 -1/4-nu/12 -1/8+3*nu/8 -1/4+nu/12 -1/8-nu/8  nu/6 1/8-3*nu/8];
+
+k_local = E/(1-nu^2) * [ ...
+    k(1) k(2) k(3) k(4) k(5) k(6) k(7) k(8)
+    k(2) k(1) k(8) k(7) k(6) k(5) k(4) k(3)
+    k(3) k(8) k(1) k(6) k(7) k(4) k(5) k(2)
+    k(4) k(7) k(6) k(1) k(8) k(3) k(2) k(5)
+    k(5) k(6) k(7) k(8) k(1) k(2) k(3) k(4)
+    k(6) k(5) k(4) k(3) k(2) k(1) k(8) k(7)
+    k(7) k(4) k(5) k(2) k(3) k(8) k(1) k(6)
+    k(8) k(3) k(2) k(5) k(4) k(7) k(6) k(1)];
+
+end
